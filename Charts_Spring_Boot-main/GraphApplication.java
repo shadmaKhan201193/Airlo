3d479e86a4b70dcf0874437809506787299b0354
shadmaKhan201193 <73832663+shadmaKhan201193@users.noreply.github.com>
2024-06-28 12:09:42 +0530

    Add files via upload

---
+++ Charts_Spring_Boot-main/GraphApplication.java
@@ -0,0 +1,21 @@
+package com.example.grafik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class GraphApplication implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrafikApplication.class, args);
+	}
+
+}
---
+++ Charts_Spring_Boot-main/README.md
@@ -0,0 +1,24 @@
+# Charts_Spring_Boot
+Bar,Pie,Line
+
+![image](https://user-images.githubusercontent.com/61595808/148648975-844decfd-0cd8-46fc-8926-4bb0c9634258.png)
+
+
+![image](https://user-images.githubusercontent.com/61595808/148648991-4691e2ca-4eac-47ed-b356-d3421e111406.png)
+
+
+![image](https://user-images.githubusercontent.com/61595808/148649031-312b041d-85eb-4d91-9b85-14a4f39d464a.png)
+
+
+
+![image](https://user-images.githubusercontent.com/61595808/148649151-3de17380-bd7c-468d-a2a3-712b7c8c08a0.png)
+
+
+
+### barGraph.html
+
+chart: { type: 'line'},
+
+
+![image](https://user-images.githubusercontent.com/61595808/148649218-fb3db2b0-dd03-4e1b-8b67-bb452b4f627f.png)
+
