73ea735dd54f53cb43699dc117499c04123c05cc
shadmaKhan201193 <73832663+shadmaKhan201193@users.noreply.github.com>
2024-06-28 12:09:05 +0530

    Add files via upload

---
+++ Chart_service-1/src/main/java/com/itl/ChartService1Application.java
@@ -0,0 +1,13 @@
+package com.itl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartService1Application.class, args);
+	}
+
+}
